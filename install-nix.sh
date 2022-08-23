0e667e070444be8bd26df061aeb6c88e2d2dc05c
Jan-Philip Loos <maxdaten@gmail.com>
2022-08-24 00:12:27 +0200

    Add install-nix helper script

---
+++ install-nix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -r -d '' HERE_CONFIG <<'EOF'
+# required
+experimental-features = nix-command flakes
+# optional optimization
+# Following: https://github.com/nix-community/nix-direnv#installation
+keep-derivations = true
+keep-outputs = true 
+warn-dirty = false
+EOF
+
+export NIX_EXTRA_CONF="$HERE_CONFIG"
+
+sh <(curl -L https://nixos.org/nix/install)
