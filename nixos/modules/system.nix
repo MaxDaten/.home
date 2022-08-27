4c572259aa007be35f0f37c1abeb751ec4481c17
Jan-Philip Loos <maxdaten@gmail.com>
2022-08-27 21:47:11 +0200

    Faster boot

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -4,6 +4,8 @@
   lib,
   ...
 }: {
+  boot.loader.timeout = 1;
+
   environment.systemPackages = with pkgs; [
     # system tools
     libraspberrypi
@@ -23,6 +25,10 @@
   users.extraUsers.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDICvbuJuq1NNPE0bPVA8s2HGh4FBGMW7mbl5iLzyDfCdvuG0LWs56OtqrnRK/d18cik3r7DIFQ2/B7d6kz2uCcUmV8BegQQ1atud532gIRMUI9s/v4zcUnmCtoUDBbWUiEJvbjNU8oJT8VxWmKnD8nSFuCpSttER7IBdB0oICEPTPvTq01pafrhD8L/L+pS4mKFHjARBuNhi7Va7TEbbIuQQgt028fMjgaL9b/dS1lHUn5Uw9yd3/MfLUS7fNhlK+cn6HvJfQL7FgH5WXZBfxiVJo1iPmFTSio6Qo7PyY27Po8zEmNA+7mNHBms4rloOGYDHmoHY1tSuc1cVfMfL/l jloos@macbook"
   ];
+
+  security = {
+    sudo.wheelNeedsPassword = false;
+  };
 
   # system.copySystemConfiguration = true;
 }
