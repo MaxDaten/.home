a7a2975ba0a6a77cd1c84b452d719cff4a4d5dc7
Jan-Philip Loos <maxdaten@gmail.com>
2022-09-24 20:59:49 +0200

    Update stateversion

--- users/jloos/home.nix
+++ users/jloos/home.nix
@@ -33,7 +33,7 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
+  home.stateVersion = "22.11";
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
