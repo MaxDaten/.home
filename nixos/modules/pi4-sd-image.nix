d70464e5a61fc03457fb39209ce8c6d64285d00a
Jan-Philip Loos <maxdaten@gmail.com>
2022-10-08 19:46:43 +0200

    Increase firmwareSize for pi

--- nixos/modules/pi4-sd-image.nix
+++ nixos/modules/pi4-sd-image.nix
@@ -12,7 +12,7 @@
     compressImage = false;
     # /boot partition
     # This might need to be increased when deploying multiple configurations.
-    firmwareSize = 512;
+    firmwareSize = 1024;
     populateRootCommands = "mkdir -p ./files/var/empty";
   };
 }
