231702e74a9ab8763b2bd8331b16fb1a65859af5
Jan-Philip Loos <maxdaten@gmail.com>
2022-10-14 23:03:58 +0200

    Configure pi4-nixos access via home-manager

--- users/jloos/home.nix
+++ users/jloos/home.nix
@@ -97,6 +97,11 @@ in
       "pi4-nixos" = {
         hostname = "pi4-nixos";
         user = "jloos";
+        identitiesOnly = true;
+        identityFile = [
+          "~/.ssh/id_ed25519"
+          "~/.ssh/id_rsa"
+        ];
       };
 
       "maandr" = {
