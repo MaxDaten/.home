cd44cd2c9b22fb03e96e0ee44f3801524e8576c8
Jan-Philip Loos <maxdaten@gmail.com>
2023-03-04 02:21:56 +0100

    Formatting

--- nixos/modules/my-networks/default.nix
+++ nixos/modules/my-networks/default.nix
@@ -29,7 +29,12 @@
       system = "x86_64-linux";
       maxJobs = 4;
       speedFactor = 2;
-      supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
     }
   ];
   nix.distributedBuilds = true;
