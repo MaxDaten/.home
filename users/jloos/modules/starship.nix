337c0fded5ac0916b894a500e9d2510683c1c2d1
Jan-Philip Loos <maxdaten@gmail.com>
2023-04-08 02:56:45 +0200

    Extract starship as module

---
+++ users/jloos/modules/starship.nix
@@ -0,0 +1,65 @@
+{pkgs, ...}: {
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+
+    # https://starship.rs/config/
+    settings = {
+      # Inserts a blank line between shell prompts
+      add_newline = true;
+
+      line_break = {disabled = false;};
+
+      format = pkgs.lib.concatStrings [
+        "$custom"
+        "$directory"
+        "$git_branch"
+        "$git_status"
+        "$package"
+        "$all"
+        "$kubernetes"
+        "$cmd_duration"
+        "$line_break"
+        "$nix_shell$shell$character"
+      ];
+
+      kubernetes = {
+        disabled = false;
+        format = "[\\[$symbol$context( \\($namespace\\))\\]]($style) ";
+        context_aliases = {
+          "k8s-cluster01.prelive.kmo.zone" = "kmo-prelive";
+          "k8s-cluster01.live.kmo.zone" = "kmo-live";
+        };
+      };
+
+      custom = {
+        direnv_basename = {
+          command = "basename \"\${DIRENV_DIR/#-/\~}\"";
+          when = "[[ -n \"$DIRENV_DIR\" ]]"; # Only show when in a direnv directory & loaded env
+          format = "[$output;]($style) ";
+          shell = ["bash"];
+          style = "red bold";
+        };
+      };
+
+      shell = {
+        fish_indicator = "🐡";
+        bash_indicator = ">_";
+        disabled = false;
+      };
+
+      docker_context = {disabled = true;};
+
+      nix_shell = {
+        format = "$symbol$state";
+        impure_msg = "";
+        pure_msg = "+";
+      };
+      gcloud.disabled = true;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
+  };
+}
