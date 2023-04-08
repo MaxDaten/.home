d51ad67923347f414e14a5eb25611684a1025315
Jan-Philip Loos <maxdaten@gmail.com>
2023-04-08 02:58:31 +0200

    Extract git module

--- users/jloos/home.nix
+++ users/jloos/home.nix
@@ -48,6 +48,7 @@ in
     imports = [
       inputs.sops-nix.homeManagerModule
       ./modules/starship.nix
+      ./modules/git.nix
     ];
 
     sops = {
@@ -262,37 +263,6 @@ in
 
     programs.direnv.enable = true;
     programs.direnv.nix-direnv.enable = true;
-
-    # Git
-    programs.git = {
-      enable = true;
-      userName = "Jan-Philip Loos";
-      userEmail = "maxdaten@gmail.com";
-
-      aliases = {
-        r = "rebase --autostash";
-        # https://softwaredoug.com/blog/2022/11/09/idiot-proof-git-aliases.html
-        mainbranch = "!git remote show origin | sed -n '/HEAD branch/s/.*: //p'";
-        switch-default = "!git switch $(git mainbranch)";
-        sd = "switch-default";
-        synced = "!git pull origin $(git mainbranch) --rebase";
-        update = "!git pull origin $(git rev-parse --abbrev-ref HEAD) --rebase";
-        squash = "!git rebase -v -i $(git mainbranch)";
-        publish = "push origin HEAD --force-with-lease";
-        pub = "publish";
-      };
-
-      extraConfig = {
-        core = {
-          autocrlf = "input";
-          excludesfile = (pkgs.writeText ".gitignore" (builtins.readFile ./global.gitignore)).outPath;
-        };
-
-        pull = {rebase = false;};
-
-        init = {defaultBranch = "main";};
-      };
-    };
 
     # Vim
     programs.neovim = {
---
+++ users/jloos/modules/git.nix
@@ -0,0 +1,31 @@
+{pkgs, ...}: {
+  programs.git = {
+    enable = true;
+    userName = "Jan-Philip Loos";
+    userEmail = "maxdaten@gmail.com";
+
+    aliases = {
+      r = "rebase --autostash";
+      # https://softwaredoug.com/blog/2022/11/09/idiot-proof-git-aliases.html
+      mainbranch = "!git remote show origin | sed -n '/HEAD branch/s/.*: //p'";
+      switch-default = "!git switch $(git mainbranch)";
+      sd = "switch-default";
+      synced = "!git pull origin $(git mainbranch) --rebase";
+      update = "!git pull origin $(git rev-parse --abbrev-ref HEAD) --rebase";
+      squash = "!git rebase -v -i $(git mainbranch)";
+      publish = "push origin HEAD --force-with-lease";
+      pub = "publish";
+    };
+
+    extraConfig = {
+      core = {
+        autocrlf = "input";
+        excludesfile = (pkgs.writeText ".gitignore" (builtins.readFile ./global.gitignore)).outPath;
+      };
+
+      pull = {rebase = false;};
+
+      init = {defaultBranch = "main";};
+    };
+  };
+}
