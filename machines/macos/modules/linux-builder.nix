1e7b74949cd7096bb46637818985f51801f0d9a1
Jan-Philip Loos <maxdaten@gmail.com>
2023-11-30 02:16:37 +0100

    feat: Update linux-builder configuration

--- machines/macos/modules/linux-builder.nix
+++ machines/macos/modules/linux-builder.nix
@@ -2,14 +2,11 @@
   nix.linux-builder.enable = true;
   nix.linux-builder.maxJobs = 8;
   # When bootstrapping, comment modules out to use the binary cache version
-  nix.linux-builder.modules = [
-    (
-      { config, ... }: {
-        virtualisation.darwin-builder.diskSize = 60 * 1024;
-        virtualisation.darwin-builder.memorySize = 16 * 1024;
-      }
-    )
-  ];
+  nix.linux-builder.config = {
+    virtualisation.darwin-builder.diskSize = 60 * 1024;
+    virtualisation.darwin-builder.memorySize = 16 * 1024;
+  };
+
   launchd.daemons.linux-builder.serviceConfig.StandardOutPath = "/var/log/linux-builder.log";
   launchd.daemons.linux-builder.serviceConfig.StandardErrorPath = "/var/log/linux-builder.log";
 }
