1fca8ab189ab2883b5eecba43ae63172eca38e6d
Jan-Philip Loos <maxdaten@gmail.com>
2024-07-12 18:25:11 +0200

    feat: Enable Fish integration and update changeDirWidgetCommand in fzf.nix module

--- users/jloos/modules/fzf.nix
+++ users/jloos/modules/fzf.nix
@@ -5,7 +5,13 @@
     in
     {
       enable = true;
+      enableFishIntegration = true;
+      # Alt-C
       changeDirWidgetCommand = "${fd} --type d --hidden --follow --exclude .git --no-ignore";
       changeDirWidgetOptions = [ "--preview 'tree -C {} | head -200'" ];
     };
+
+  programs.fsih.interactiveShellInit = ''
+    set --global --export FZF_CTRL_T_OPTS "--walker-skip .git,node_modules,target --preview 'bat --style=number,header,grid --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+  '';
 }
