280ae66b3ae99e06846a0c8bfc80f633d68f2226
Jan-Philip Loos <maxdaten@gmail.com>
2024-07-31 23:24:51 +0200

    chore: Enable nix.linux-builder and update configuration in linux-builder.nix

--- hosts/macos/modules/linux-builder.nix
+++ hosts/macos/modules/linux-builder.nix
@@ -1,8 +1,10 @@
 { ... }: {
+  # sudo launchctl kickstart -k system/org.nixos.linux-builder
+  # https://github.com/LnL7/nix-darwin/blob/master/modules/nix/linux-builder.nix
   nix.linux-builder.enable = true;
   nix.linux-builder.maxJobs = 8;
   # When bootstrapping, comment modules out to use the binary cache version
-  nix.linux-builder.config = ({pkgs, ...}: {
+  nix.linux-builder.config = ({ pkgs, ... }: {
     # https://github.com/NixOS/nixpkgs/blob/master/doc/packages/darwin-builder.section.md#darwinlinux-builder-sec-darwin-builder
     virtualisation.darwin-builder.diskSize = 60 * 1024;
     virtualisation.darwin-builder.memorySize = 16 * 1024;
