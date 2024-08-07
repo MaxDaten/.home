9db59ba5d3b7c8ba98eb59e735487144a7aa81f5
Jan-Philip Loos <maxdaten@gmail.com>
2024-08-07 05:44:45 +0200

    chore: Enable ephemeral mode in linux-builder.nix

--- hosts/macos/modules/linux-builder.nix
+++ hosts/macos/modules/linux-builder.nix
@@ -3,6 +3,7 @@
   # https://github.com/LnL7/nix-darwin/blob/master/modules/nix/linux-builder.nix
   nix.linux-builder.enable = true;
   nix.linux-builder.maxJobs = 8;
+  nix.linux-builder.ephemeral = true;
   # When bootstrapping, comment modules out to use the binary cache version
   nix.linux-builder.config = ({ pkgs, ... }: {
     # https://github.com/NixOS/nixpkgs/blob/master/doc/packages/darwin-builder.section.md#darwinlinux-builder-sec-darwin-builder
