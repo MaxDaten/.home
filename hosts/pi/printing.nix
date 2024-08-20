22522083f8989ff241461aab0800e0bc0accef8d
Jan-Philip Loos <maxdaten@gmail.com>
2024-08-20 06:20:51 +0200

    refactor: Update printing.nix to include sub_filter for /printing/ URLs

--- hosts/pi/printing.nix
+++ hosts/pi/printing.nix
@@ -30,8 +30,13 @@ in
   # nginx reverse proxy from /printing to localhost:631
   services.nginx.virtualHosts."${config.networking.fqdnOrHostName}" = lib.mkIf config.services.nginx.enable {
     locations."/printing/" = {
-      proxyPass = "http://[::1]:${toString port}";
+      proxyPass = "http://127.0.0.1:${toString port}/";
       recommendedProxySettings = true;
+      extraConfig = ''
+        sub_filter 'href="/' 'href="/printing/';
+        sub_filter 'src="/' 'src="/printing/';
+        sub_filter_once off;
+      '';
     };
   };
 }
