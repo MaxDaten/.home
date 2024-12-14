5d6a6554a19288226a0dbabf7ffd1e142e89be77
Jan-Philip Loos <maxdaten@gmail.com>
2024-12-14 02:24:59 +0100

    fix: disable failing linux-builder
    fails with:
    
    Unexpected error in object_property_find_err() at ../qom/object.c:1358:
    qemu-system-aarch64: Property 'max-arm-cpu.sme' not found

--- hosts/macos/modules/linux-builder.nix
+++ hosts/macos/modules/linux-builder.nix
@@ -1,14 +1,16 @@
-{ ... }: {
+{ pkgs, ... }: {
   # sudo launchctl kickstart -k system/org.nixos.linux-builder
   # https://github.com/LnL7/nix-darwin/blob/master/modules/nix/linux-builder.nix
   nix.linux-builder = {
-    enable = true;
+    # Currently fails with qemu-system-aarch64:
+    # Property 'max-arm-cpu.sme' not found
+    enable = false;
+
     ephemeral = true;
-    systems = [ "x86_64-linux" ];
+    systems = [ "x86_64-linux" "aarch64-linux" ];
     # When bootstrapping, comment modules out to use the binary cache version
     config = ({ ... }: {
-      nixpkgs.hostPlatform = "x86_64-linux";
-      virtualisation.cores = 10;
+      virtualisation.cores = 8;
       # # https://github.com/NixOS/nixpkgs/blob/master/doc/packages/darwin-builder.section.md#darwinlinux-builder-sec-darwin-builder
       virtualisation.darwin-builder.diskSize = 60 * 1024;
       virtualisation.darwin-builder.memorySize = 16 * 1024;
