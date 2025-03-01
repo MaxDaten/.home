012d3c656079db3c4554fb07ae5b725510ce80df
Jan-Philip Loos <maxdaten@gmail.com>
2025-03-01 02:20:59 +0100

    chore: migrate touchIdAuth config

--- hosts/macos/configuration.nix
+++ hosts/macos/configuration.nix
@@ -41,7 +41,7 @@
   environment.variables.LANG = "en_US.UTF-8";
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   services.ollama.enable = true;
 }
