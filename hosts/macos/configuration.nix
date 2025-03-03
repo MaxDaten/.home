b375935946d664526c19f965617f425294398285
Jan-Philip Loos <maxdaten@gmail.com>
2025-03-03 21:50:46 +0100

    chore: Remove comment

--- hosts/macos/configuration.nix
+++ hosts/macos/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   # https://daiderd.com/nix-darwin/manual/index.html
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
@@ -40,7 +40,6 @@
   '';
   environment.variables.LANG = "en_US.UTF-8";
 
-  # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
   services.ollama.enable = true;
