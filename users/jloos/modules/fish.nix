c6a1d27b29af59e0fe1d297ffa3042347b848b44
Jan-Philip Loos <maxdaten@gmail.com>
2025-03-04 01:43:53 +0100

    chore: Add github pat

--- users/jloos/modules/fish.nix
+++ users/jloos/modules/fish.nix
@@ -84,7 +84,8 @@ in {
       set -U PROJECT_PATHS ~/Developer/maxdaten-io/buzzar ~/Developer/maxdaten-io/gitops ~/Workspace/.home ~/Developer ~/Developer/medosync
       set __done_enabled
 
-      set -gx GITHUB_TOKEN (cat ${config.sops.secrets.GITHUB_TOKEN.path})
+      # Currently disabled because PAT not working for oauth, use gh auth login --web -h github.com
+      # set -gx GITHUB_TOKEN (cat ${config.sops.secrets.GITHUB_TOKEN.path})
     '';
 
     functions.fish_reload = "source ~/.config/fish/config.fish";
