528c511cc577264981fc76e10de107f3ec5ac971
Jan-Philip Loos <maxdaten@gmail.com>
2025-07-11 01:16:30 +0200

    fix shellcheck

--- generate-sops-keys.sh
+++ generate-sops-keys.sh
@@ -3,8 +3,8 @@
 SOPS_AGE_KEY_DIRECTORY="${HOME}/.config/sops/age"
 echo "Generate key in ${SOPS_AGE_KEY_DIRECTORY}"
 
-mkdir -p $SOPS_AGE_KEY_DIRECTORY
-ssh-to-age -private-key -i ~/.ssh/id_ed25519 >$SOPS_AGE_KEY_DIRECTORY/keys.txt
+mkdir -p "$SOPS_AGE_KEY_DIRECTORY"
+ssh-to-age -private-key -i ~/.ssh/id_ed25519 >"$SOPS_AGE_KEY_DIRECTORY/keys.txt"
 
 echo "Public key:"
-age-keygen -y $SOPS_AGE_KEY_DIRECTORY/keys.txt
+age-keygen -y "$SOPS_AGE_KEY_DIRECTORY/keys.txt"
